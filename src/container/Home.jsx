809e38dd2ef0c2bb06d69b8478fb53b6d46cd04e
bsj <tomasconcubi@gmail.com>
2023-10-13 21:26:34 -0400

    fixed mobile top

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -44,7 +44,7 @@ const Home = () => {
             <img src={logo} alt="logo" className="w-28" />
           </Link>
           <Link to={`user-profile/${user?._id}`}>
-            <img src={user?.image} alt="logo" className="w-28" />
+            <img src={user?.image} alt="logo" className="w-12 rounded-full" />
           </Link>
         </div>
       </div>
